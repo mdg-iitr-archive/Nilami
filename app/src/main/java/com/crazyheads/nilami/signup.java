13acee54f58fa428a59bcbf16f5b2bde297f44cf
leshnabalara <leshnabalara2001@gmail.com>
2018-12-10 20:04:40 +0530

    login and sign-up layouts

---
+++ app/src/main/java/com/crazyheads/nilami/signup.java
@@ -0,0 +1,13 @@
+package com.crazyheads.nilami;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class signup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
