c0c8c096677b01932f5e86a4520bb50bc2506d7f
leshnabalara <leshnabalara2001@gmail.com>
2018-12-19 15:00:51 +0530

    migrate to androidx

--- app/src/main/java/com/crazyheads/nilami/signup.java
+++ app/src/main/java/com/crazyheads/nilami/signup.java
@@ -3,7 +3,7 @@ package com.crazyheads.nilami;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
---
+++ app/src/main/java/com/crazyheads/nilami/userprofile.java
@@ -0,0 +1,62 @@
+package com.crazyheads.nilami;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class userprofile extends Activity {
+
+
+    private static final int CHOOSE_IMAGE = 101;
+    ImageView imageView;
+    EditText editTextDisplayName;
+    Uri uriProfileImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+
+        imageView=(ImageView)findViewById(R.id.imageView);
+        editTextDisplayName=(EditText)findViewById(R.id.editTextDisplayName);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==CHOOSE_IMAGE && resultCode==RESULT_OK && data!=null && data.getData()!=null)
+        {
+           uriProfileImage= data.getData();
+
+            try {
+                Bitmap bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),uriProfileImage);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void oncameraclick(View view) {
+        showImageChooser();
+    }
+
+    private void showImageChooser()
+    {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Profile Image"),CHOOSE_IMAGE);
+    }
+}
